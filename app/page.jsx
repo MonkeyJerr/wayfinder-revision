22a6247ed2274d911402a5a1319fa7901ce8a888
Alexander Ma <ajm3@andrew.cmu.edu>
2023-06-22 14:03:59 -0400

    adding files

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Player from './player.jsx'
+
+const page = () => {
+  return (
+    <div>
+    <div className="underline">Hello World</div>
+    <Player/>
+    </div>
+  )
+}
+
+export default page
